d1ef71d4c147e23079d9396c6e5a45420b4a0dca
Raj <raj.bharamani@datacurate.ai>
2025-07-29 10:41:21 +0530

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-echo "Pulling latest changes from prod branch..."
-cd /home/azureuser/aayu_app/fastapi
 
+cd /home/azureuser/aayu_app  # Not fastapi — go to the repo root
+
+echo "==== Directory ===="
+pwd
+echo "==== Git Remote ===="
+git remote -v
+echo "==== Branch ===="
+git branch
+
+echo "Pulling latest changes from prod branch..."
 git fetch origin prod
 git reset --hard origin/prod
 
